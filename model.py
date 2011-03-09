1565114b3b46171b478a5df988987dbe029a1335
Anand Patil <anand.prabhakar.patil@gmail.com>
2011-03-09 16:40:30 +0000

    Aaahh, so nice.

--- model.py
+++ model.py
@@ -8,28 +8,37 @@ from pysistence import func
 # Variables 
 
 def empty_model():
+    "Initializes a new empty model."
     return ps.make_dict(variables=ps.make_list(), factors=ps.make_list(T.constant(0)), stream=th.tensor.shared_randomstreams.RandomStreams())
 
 def add_stochastic(model, name, new_variable, new_factors):
+    "Returns a stochastic variable, and a version of model that incorporates that variable."
     new_variable.name = name
     return new_variable, model.using(variables = model['variables'].cons(new_variable),
                         factors = model['factors'].concat(ps.make_list(*new_factors)))
 
-add_deterministic = functools.partial(add_stochastic, new_factors=[])
+def add_deterministic(model, name, new_variable):
+    "Returns a deterministic variable, and a version of the model that incorporates that variable."
+    return add_stochastic(model, name, new_variable, new_factors)
                         
 def isstochastic(variable):
+    "Tests whether the variable is stochastic conditional on its parents. WARNING: this implementation may be too brittle."
     return hasattr(variable,'rng')
     
 def isdeterministic(variable):
+    "Tests whether the variable is deterministic conditional on its parents."
     return not isstochastic(variable)
 
 def seed(variables, seed):
-    [v.rng.value.seed(seed) for v in variables]
+    "Seeds all the rngs used by the variables."
+    [v.rng.value.seed(seed) for v in filter(isstochastic,variables)]
 
 def stochastics(model):
+    "Returns all the stochastic variables in the model."
     return filter(isstochastic, model['variables'])
     
 def deterministics(model):
+    "Returns all the deterministic variables in the model."
     return filter(isdeterministic, model['variables'])
 
 def logp(model, arguments=[]):
