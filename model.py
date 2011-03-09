212ed5abe87ea5abed95b3774544db2ebcf14699
Anand Patil <anand.prabhakar.patil@gmail.com>
2011-03-09 16:16:45 +0000

    Model definition in place

--- model.py
+++ model.py
@@ -17,13 +17,6 @@ def add_stochastic(model, name, new_variable, new_factors):
 def add_deterministic(model, name, new_variable):
     return add_stochastic(model, name, new_variable, [])
                         
-def add_normal(model, name, m, v):
-    new_var = model['stream'].normal(avg=m, std=T.sqrt(v))
-    new_factors = [T.log(2*np.pi), 
-                    -T.prod(T.shape(new_var))*T.log(v)/2,
-                    -(new_var-m)**2/2/v]
-    return add_stochastic(model, name, new_var, new_factors)
-
 def isstochastic(variable):
     return hasattr(variable,'rng')
     
@@ -46,14 +39,4 @@ def logp(model, variables=None):
         if any(map(isdeterministic, variables)):
             raise TypeError, "The arguments of the logp function cannot include the deterministics %s."%filter(isdeterministic, variables)
         return th.function(variables, T.sum(list(model['factors'])), no_default_updates=True)
-    
-if __name__ == '__main__':
-    m = empty_model()
-    x, m2 = add_normal(m,'x',0,1)
-    z, m3 = add_deterministic(m2,'z',x+3)
-    y, m4 = add_normal(m3,'y',z,100)
-    f = logp(m4)
-    f2 = logp(m4, [x])
-    seed(stochastics(m4), 3)
-    print f()
     
